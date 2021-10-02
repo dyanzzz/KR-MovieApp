8aad8fc75ca4a6cccb84436cc8a99cdbf0504506
Dian Tri Budianto <dyanzzz@gmail.com>
2021-10-03 04:43:17 +0700

    add api interface to connection on api moviedb

---
+++ app/src/main/java/com/kotlin/movieapp/data/remote/MovieApi.kt
@@ -0,0 +1,18 @@
+package com.kotlin.movieapp.data.remote
+
+import com.kotlin.movieapp.BuildConfig
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MovieApi {
+    companion object{
+        const val BASE_URL = "https://api.themoviedb.org/3/"
+        const val API_KEY = BuildConfig.MOVIEDB_API_KEY
+    }
+
+    @GET("movie/now_playing?api_key=$API_KEY")
+    suspend fun getNowPlayingMovies(
+        @Query("page")
+        position: Int
+    ): MovieResponse
+}
---
+++ app/src/main/java/com/kotlin/movieapp/data/remote/MovieResponse.kt
@@ -0,0 +1,5 @@
+package com.kotlin.movieapp.data.remote
+
+data class MovieResponse(
+    val results: List<Movie>
+)
