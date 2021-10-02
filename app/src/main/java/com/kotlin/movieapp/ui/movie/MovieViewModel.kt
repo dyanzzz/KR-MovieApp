8ae023a312b15ca999393ebda8cce87a07e1c6b2
Dian Tri Budianto <dyanzzz@gmail.com>
2021-10-03 05:17:32 +0700

    add paging source

--- app/src/main/java/com/kotlin/movieapp/ui/movie/MovieViewModel.kt
+++ app/src/main/java/com/kotlin/movieapp/ui/movie/MovieViewModel.kt
@@ -4,5 +4,6 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import com.kotlin.movieapp.data.remote.MovieRepository
 
-class MovieViewModel @ViewModelInject constructor(private val repository: MovieRepository): ViewModel() {
+class MovieViewModel @ViewModelInject constructor(repository: MovieRepository): ViewModel() {
+    val movies = repository.getNowPlayingMovies()
 }
